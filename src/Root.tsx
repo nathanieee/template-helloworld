badff89b7bbaf926923e1e36f0d6e6904e9aba7e
nathanieee <nthn145975@gmail.com>
2026-01-31 22:45:34 +0800

    feat: add Kurzgesagt2Minute composition to Root

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { Composition } from "remotion";
-import { MainVideo } from "./compositions/MainVideo";
+import { MainVideo, Kurzgesagt2Minute } from "./compositions";
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
+      {/* Original 5-minute video */}
       <Composition
         id="MainVideo"
         component={MainVideo}
         durationInFrames={9000}
+        fps={30}
+        width={1920}
+        height={1080}
+        defaultProps={{}}
+      />
+
+      {/* New 2-minute Kurzgesagt-style video */}
+      <Composition
+        id="Kurzgesagt2Minute"
+        component={Kurzgesagt2Minute}
+        durationInFrames={3600}
         fps={30}
         width={1920}
         height={1080}
